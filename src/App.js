231418fbcc7fec8af7af235ec5ebf5fa53b7cc6d
alexander mitchell <greatful_dad@icloud.com>
2022-03-24 16:31:35 -0500

    functioning

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ const tasks = [
         id: 4,
         description: 'learn to rest'
     },
+];
 
-]
 function App() {
     const [todos, setTodos] = useState(tasks);
     const [inputValue, setInputValue] = useState("");
@@ -33,15 +33,12 @@ function App() {
     const handleSubmit = (e) => {
         e.preventDefault();
         setTodos([...todos, { description: inputValue }]);
-        setInputValue("");
     };
 
     return (
-        //fragment that we use to wrap is basically a div
-        //but it's  a transparent div
-        <>
+      <>
           <div className='navbar'>myNavbar</div>
-            <div className='App'>
+          <div className='App'>
             <h1>My Todo List</h1>
             <form onSubmit={handleSubmit}>
                     <input
@@ -51,11 +48,11 @@ function App() {
                         value={inputValue}
                         onChange={handleChange}
                     />
-              <input type="button" value="Add Task" />
+               <input type="button" value="Add Task" />
             </form>
-                {todos.map((task, index) => (
-                    <Task taskInfo={task} key={index} />
-                ))}
+              {todos.map((task, index) => (
+                <Task taskInfo={task} key={index} />
+            ))}
           </div>
         </>
     );
